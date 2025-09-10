2785744984f637b19231934ffd32aabbc00d7e6b
dara1141 <darina2.bajusova@gmail.com>
2025-09-10 18:24:58 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,52 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+//ffff
+
+//darka zmena
+//darka zmena 2
+/////
+///
+///6666da
+///dd
+
+//**
+
+//22.08 3
+//22
+
+// 22.08
+// 22.08 2
+
+// zmena 1
+
+// zmena 2
+
+// zmena 4
+
+// zmena 4 b
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
